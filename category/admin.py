e03b2f094456d73e8adc599f75ec89d862a905b4
athulya3404 <athulya3404@gmail.com>
2026-08-17 12:54:09 +0530

    Implement category management

---
+++ category/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+
+from .models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "is_active",
+        "created_at",
+        "updated_at",
+    )
+
+    list_filter = (
+        "is_active",
+    )
+
+    search_fields = (
+        "name",
+        "description",
+    )
+
+    ordering = (
+        "name",
+    )
